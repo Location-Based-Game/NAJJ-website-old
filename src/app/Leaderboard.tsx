2ca017a25b2de7485fe31303fa2d61d967d75411
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-05-31 00:13:40 -0500

    leaderboard

---
+++ src/app/Leaderboard.tsx
@@ -0,0 +1,52 @@
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { db } from "./firebaseConfig";
+
+type ScoreData = {
+  username: string;
+  highScore: number;
+};
+
+export default async function Leaderboard() {
+  const scores = collection(db, "leaderboard");
+  const q = query(scores, orderBy("highScore"));
+  const querySnapshot = await getDocs(q);
+
+  const data: ScoreData[] = [];
+  querySnapshot.forEach((doc) => {
+    data.push(doc.data() as ScoreData);
+  });
+
+  data.reverse();
+
+  return (
+    <div className="w-full flex flex-col items-center py-20 h-full">
+      <div className="w-full xl:w-[900px] px-12">
+        {data.map((e, i) => {
+          return <Item key={i} index={i} data={e} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+interface Item {
+  data: ScoreData;
+  index: number;
+}
+
+function Item({ data, index }: Item) {
+  return (
+    <div
+      key={index}
+      className={`flex justify-between bg-black px-16 py-4 ${
+        index % 2 ? "bg-opacity-50" : "bg-opacity-40"
+      }`}
+    >
+      <div className="flex gap-10">
+        <div className="w-5 font-bold text-right">{index + 1}</div>
+        <div>{data.username}</div>
+      </div>
+      <div>{data.highScore}</div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,68 @@
 import Image from "next/image";
+import Leaderboard from "./Leaderboard";
 
 export default function Home() {
   return (
-    <main>
-      <div className="w-screen h-screen relative">
-        <Image
-          src={"/images/splash.jpg"}
-          alt="SPEEED! splash image"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: '100%', height: 'auto' }}
-          priority
-        />
-      </div>
-    </main>
+    <>
+      <div className="splashBackground w-screen h-screen fixed"></div>
+      <main className="flex w-full">
+        <nav className="sticky top-0 h-screen bg-black bg-opacity-80 backdrop-blur-lg min-w-[16rem]">
+          <ul className="py-16">
+            <ListItem label="Play" href="#play" />
+            <ListItem label="Tutorial" href="#leaderboard" />
+            <ListItem label="Leaderboard" href="#leaderboard" />
+            <ListItem label="About" href="#leaderboard" />
+          </ul>
+        </nav>
+        <div className="w-full">
+          <section id="play" className="w-full h-screen flex flex-col xl:items-end xl:flex-row p-16 gap-10">
+            <div className="grow">
+              <Image
+                src={"/images/SPEEED Logo@4x.webp"}
+                alt="SPEEED Logo"
+                width={500}
+                height={500}
+              />
+            </div>
+            <Image
+              src={"/images/googleplay.png"}
+              alt="SPEEED Logo"
+              width={200}
+              height={200}
+            />
+            <Image
+              src={"/images/appstore.png"}
+              alt="SPEEED Logo"
+              width={200}
+              height={200}
+            />
+          </section>
+          <section
+            id="leaderboard"
+            className="w-full bg-black bg-opacity-30 h-screen backdrop-blur-lg"
+          >
+            <Leaderboard />
+          </section>
+        </div>
+      </main>
+    </>
+  );
+}
+
+interface ListItem {
+  label: string;
+  href: string;
+}
+
+function ListItem({ label, href }: ListItem) {
+  return (
+    <li>
+      <a
+        href={href}
+        className="w-full block px-16 py-4 transition-colors hover:bg-opacity-30 hover:bg-black active:bg-opacity-50"
+      >
+        {label}
+      </a>
+    </li>
   );
 }
