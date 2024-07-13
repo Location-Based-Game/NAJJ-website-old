2e0cbcc1b1c2fb40c5e455fe075d8b0faac0b794
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-07-13 01:08:00 -0500

    fix firestore export

--- src/app/firebaseConfig.ts
+++ src/app/firebaseConfig.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,4 +19,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const db = getDatabase(app);
+export const db = getFirestore(app)
