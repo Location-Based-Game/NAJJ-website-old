f700315b1ac84f1ce1263239391bca3a3c871fbd
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-07-14 23:03:47 -0500

    add leaderboard heading

--- src/app/speeed/Leaderboard.tsx
+++ src/app/speeed/Leaderboard.tsx
@@ -20,7 +20,10 @@ export default async function Leaderboard() {
 
   return (
     <div className="w-full flex flex-col items-center py-20 h-full">
-      <div className="w-full xl:w-[900px] px-12">
+      <h2 className="font-header w-full text-center text-4xl bg-gradient-to-b from-white from-70% to-transparent text-transparent bg-clip-text">
+        Leaderboard
+      </h2>
+      <div className="w-full mt-8 xl:w-[900px] px-12">
         {data.map((e, i) => {
           return <Item key={i} index={i} data={e} />;
         })}
